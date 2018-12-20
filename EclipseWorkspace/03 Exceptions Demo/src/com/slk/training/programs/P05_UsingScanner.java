b9e17c70dc1265d020561d763180f9f55d3a286d
kayartaya.vinod <kayartaya.vinod@gmail.com>
2018-12-20 12:39:57 +0530

    🇮🇳

--- EclipseWorkspace/03 Exceptions Demo/src/com/slk/training/programs/P05_UsingScanner.java
+++ EclipseWorkspace/03 Exceptions Demo/src/com/slk/training/programs/P05_UsingScanner.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class P05_UsingScanner {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); // able to read from keyboard buffer
 			
